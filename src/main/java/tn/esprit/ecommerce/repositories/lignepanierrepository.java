6afb9555f44d4f5f9294cf6f4edf2a17e54bd92b
walidbarhoumi <100834407+walidbarhoumi@users.noreply.github.com>
2025-04-25 13:53:37 +0100

    Final test

--- src/main/java/tn/esprit/ecommerce/repositories/lignepanierrepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.ecommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.ecommerce.entities.lignepanier;
-
-public interface lignepanierrepository extends JpaRepository<lignepanier, Long> {
-}
